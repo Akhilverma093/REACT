0cea883c6af213432bcd018faa31c3777dfe01f4
Akhilverma093 <akhilver093@gmail.com>
2026-03-10 02:03:11 +0530

    day2 complete -components

---
+++ 02-DAY/src/components/card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+            <h1>I AM AKHIL VERMA</h1>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis odit optio nihil iste, voluptatibus debitis nulla commodi dolorem nobis libero quis ad inventore maiores molestiae adipisci sit, eum ratione illum?</h2>
+              
+            
+          </div> 
+    </div>
+  )
+}
+
+export default card
+
